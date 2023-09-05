fb15538825bf80ab520d39000e25f7c81288420e
AlvaroDelgadilloM <78710308+AlvaroDelgadilloM@users.noreply.github.com>
2023-09-05 15:11:53 -0600

    ejmeplo de app
    app en thymleaf

---
+++ src/main/java/com/ccs/swat/iaas/myApp/MyAppApplication.java
@@ -0,0 +1,15 @@
+package com.ccs.swat.iaas.myApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.ccs.swat.iaas")
+public class MyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppApplication.class, args);
+	}
+
+}
